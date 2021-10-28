6bc4cc428d457f5398c8542cdaec53fe48d42154
Lilian Umeakunne <73794416+Lilian-Chinelo@users.noreply.github.com>
2021-10-28 16:27:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+print("Welcome to the ticket portal.")
+height = int(input("Please enter your height in cm: "))
+age = int(input("How old are you?: "))
+
+photo_price = 3
+bill = 0
+
+
+if height >= 120:
+  
+  if age <= 12 :
+    bill = 5
+    print(f"Child tickets cost ${bill}")
+
+  if age <= 18:
+    bill = 7
+    print(f"Youth ticket cost ${bill}")
+
+  if age > 18:
+    bill = 12
+    print(f"Adult ticket cost ${bill}")
+
+  photo_consent = input("Do you want a photo of you taken? Yes or No :").lower()
+
+  if photo_consent == "yes":
+    #Add $3 to their bill.
+    print(f"Total to pay is {(bill + photo_price)}")
+  else:
+    print(f"Total to pay is {bill}")
+
+else:
+  print("Sorry, you need to grow taller before you can ride. ")
+
+  
